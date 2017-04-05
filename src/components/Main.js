fd1286a34bc5378cb41ad58975f7dc979a64bd6d
TheSmallMonster <395041293@qq.com>
2017-04-05 23:40:22 +0800

    调整项目结构

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import header from '../style/header.less';
-
 class AppComponent extends React.Component {
   render() {
     return (
-      <div style={header}>
-        <div >APPLICATION CONTENT</div>
+      <div >
       </div>
     );
   }
