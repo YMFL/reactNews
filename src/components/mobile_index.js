80b33e3631a6505ab93ae29e19749ed52463f420
TheSmallMonster <395041293@qq.com>
2017-04-09 16:03:30 +0800

    mobile端首页模块开发

--- src/components/mobile_index.js
+++ src/components/mobile_index.js
@@ -4,41 +4,57 @@
 import React from 'react';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
-import {Tabs} from 'antd';
+import {Tabs,Carousel} from 'antd';
 
 import '../style/mobile.less';
+import MobileList from './mobile_list';
 
 const TabPane =Tabs.TabPane;
 
 class MoblieIndex extends React.Component {
   render() {
+    const setting={
+      dots:true,
+      speed:500,
+      infinite:true,
+      slidesToShow:1,
+      autoplay:true
+    }
     return (
       <div>
         <MobileHeader></MobileHeader>
         <Tabs >
           <TabPane tab="头条" key="1">
-
+            <div className="carousel" >
+              <Carousel {...setting} >
+                <div><img src="../images/carousel_1.jpg" alt="carousel_1.jpg"/></div>
+                <div><img src="../images/carousel_2.jpg" alt="carousel_2.jpg"/></div>
+                <div><img src="../images/carousel_3.jpg" alt="carousel_3.jpg"/></div>
+                <div><img src="../images/carousel_4.jpg" alt="carousel_4.jpg"/></div>
+              </Carousel>
+            </div>
+            <MobileList count="20" type="top" />
           </TabPane>
           <TabPane tab="社会" key="2">
-
+            <MobileList count="20" type="shehui" />
           </TabPane>
           <TabPane tab="国内" key="3">
-
+            <MobileList count="20" type="guonei" />
           </TabPane>
           <TabPane tab="国际" key="4">
-
+            <MobileList count="20" type="guoji" />
           </TabPane>
           <TabPane tab="娱乐" key="5">
-
+            <MobileList count="20" type="yule" />
           </TabPane>
           <TabPane tab="体育" key="6">
-
+            <MobileList count="20" type="tiyu" />
           </TabPane>
           <TabPane tab="科技" key="7">
-
+            <MobileList count="20" type="keji" />
           </TabPane>
           <TabPane tab="时尚" key="8">
-
+            <MobileList count="20" type="shishang" />
           </TabPane>
         </Tabs>
         <MobileFooter/>
--- src/components/mobile_list.js
+++ src/components/mobile_list.js
@@ -2,15 +2,63 @@
  * Created by Administrator on 2017/4/8.
  */
 import React from 'react';
-class AppComponent extends React.Component {
+import {Row, Col} from 'antd';
+import {Router, Route, hasHistory, Link} from 'react-router';
+
+class MobileList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      news: ''
+    }
+  };
+
+  componentWillMount() {
+    console.log(this.props.type)
+    var myFetchOptions = {
+      method: 'GET'
+    };
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" + this.props.type + "&count=" + this.props.count, myFetchOptions)
+      .then(response => response.json())
+      .then(json => this.setState({news: json}))
+  };
+
   render() {
+    const {news}=this.state;
+    const newsList = news.length ?
+      news.map((newsItem, index) => (
+        //遍历  要给key  不然会有警告
+        <section key={index} className="m_article list-item special_section clearfix">
+          <Link to={'details/${newsItem.uniquekey}'}>
+            <div className="m_article_img">
+              <img src={newsItem.thumbnail_pic_s} alt={newsItem.uniquekey}/>
+            </div>
+            <div className="m_article_info">
+              <div className="m_article_title">
+                <span>{newsItem.title}</span>
+              </div>
+              <div className="m_article_desc clearfix">
+                <div className="m_article_desc_l">
+                  <span className="m_article_channel">{newsItem.realtype}</span>
+                  <span className="m_article_time">{newsItem.date}</span>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </section>
+      )) : '没有加载到任何新闻';
+
     return (
-      <div>
+      <div className="topNewsList">
+        <Row>
+          <Col span={24}>
+            {newsList}
+          </Col>
+        </Row>
       </div>
     );
   }
 }
-AppComponent.defaultProps = {
-};
+MobileList.defaultProps = {};
 
-export default AppComponent;
+export default MobileList;
