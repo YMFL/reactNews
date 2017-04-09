387d9872a08b51b6041816020d46539564259cc5
TheSmallMonster <395041293@qq.com>
2017-04-09 15:01:22 +0800

    PC端首页模块开发

--- src/components/pc_news_image_block.js
+++ src/components/pc_news_image_block.js
@@ -2,68 +2,69 @@
  * Created by Administrator on 2017/4/8.
  */
 import React from 'react';
-import { Card } from 'antd';
+import {Card} from 'antd';
 import {Router, Route, hasHistory, Link} from 'react-router';
 
 class PCNewsImageBlock extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      news:''
+    this.state = {
+      news: ''
     }
   };
+
   componentWillMount() {
     var myFetchOptions = {
       method: 'GET'
     };
-      fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type="+ this.props.type+"&count="+this.props.count,myFetchOptions)
-      .then(response=>response.json())
-      .then(json=>this.setState({news:json}))
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" + this.props.type + "&count=" + this.props.count, myFetchOptions)
+      .then(response => response.json())
+      .then(json => this.setState({news: json}))
   };
+
   render() {
-    const styleImage={
-      display:'block',
-      width:this.props.imageWidth,
-      height:'90px'
+    const styleImage = {
+      display: 'block',
+      width: this.props.imageWidth,
+      height: '90px'
     };
-    const styleH3={
-      width:this.props.imageWidth,
-      whiteSpace:'nowrap',
-      overflow:'hidden',
-      textOverflow:'ellipsis'
+    const styleH3 = {
+      width: this.props.imageWidth,
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis'
     };
-    const styleP= {
-      width:'100px',
-      whiteSpace:'nowrap',
-      overflow:'hidden',
-      textOverflow:'ellipsis'
+    const styleP = {
+      width: '100px',
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis'
     };
     const {news}=this.state;
-    const newsList=news.length ?
-      news.map((newsItem,index)=>
+    const newsList = news.length ?
+      news.map((newsItem, index) =>
         <div key={index} className="imageblock">
-          <Link to={'details/${newsItem.uniquekey}'} target="_blank" >
-           <div className="custom-image">
-             <img src={newsItem.thumbnail_pic_s} alt="" style={styleImage}/>
-           </div>
+          <Link to={'details/${newsItem.uniquekey}'} target="_blank">
+            <div className="custom-image">
+              <img src={newsItem.thumbnail_pic_s} alt="" style={styleImage}/>
+            </div>
             <div className="custom-card">
               <h3 style={styleH3} title={newsItem.title}>{newsItem.title} </h3>
               <p style={styleP}>{newsItem.author_name}</p>
             </div>
           </Link>
         </div>
-      ) :'没有加载到任何新闻';
+      ) : '没有加载到任何新闻';
 
     return (
       <div className="topNewsList">
-        <Card title={this.props.cardTitle} bordered={true} style={{width:this.props.width}}>
+        <Card title={this.props.cardTitle} bordered={true} style={{width: this.props.width}}>
           {newsList}
         </Card>
       </div>
     );
   }
 }
-PCNewsImageBlock.defaultProps = {
-};
+PCNewsImageBlock.defaultProps = {};
 
 export default PCNewsImageBlock;
